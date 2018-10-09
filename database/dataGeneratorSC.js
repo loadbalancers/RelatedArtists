5c3991a6a38869bab0dcd5134e4b549992d4f94f
hellohello810 <iloveporkchop810@gmail.com>
2018-10-09 14:46:51 -0700

    s

--- database/dataGeneratorSC.js
+++ database/dataGeneratorSC.js
@@ -21,42 +21,42 @@ const generateArtist = () => new Promise ((resolve, reject) => {
 
 //--------------------------relationships
 
-const generateArtistRelationships = () => new Promise ((resolve, reject) => {
-  let artists ='';
-  for (var i = 9000001; i <= 10000000; i ++) {
-    artists += `${i}, ${faker.random.number({min:1, max:10000000})}\n`
-  }
-  resolve(fs.appendFileSync('./database/relations.csv', artists));
-}); 
+// const generateArtistRelationships = () => new Promise ((resolve, reject) => {
+//   let artists ='';
+//   for (var i = 9000001; i <= 10000000; i ++) {
+//     artists += `${i}, ${faker.random.number({min:1, max:10000000})}\n`
+//   }
+//   resolve(fs.appendFileSync('./database/relations.csv', artists));
+// }); 
 
-(function genRelations() {
-  for (var i = 0; i < 10; i ++) {
-    generateArtistRelationships();
-  }
-}());
+// (function genRelations() {
+//   for (var i = 0; i < 10; i ++) {
+//     generateArtistRelationships();
+//   }
+// }());
 
 // ---------------------------
 // experiment... 
 
-const genArtistRelationships = (stream, start, end) => new Promise ((res, rej) => {
-  for (var i = start; i <= end; i ++) {
-    stream.write(`${i}, ${Math.floor(Math.random()*10000000)}\n`);
-  }
-  stream.on('error', err => rej(err));
-  stream.end(res)
-});
+// const genArtistRelationships = (stream, start, end) => new Promise ((res, rej) => {
+//   for (var i = start; i <= end; i ++) {
+//     stream.write(`${i}, ${Math.floor(Math.random()*10000000)}\n`);
+//   }
+//   stream.on('error', err => rej(err));
+//   stream.end(res)
+// });
 
-const genRelations = async () => {
-  k = 1000000;
-  start = 1;
-  end = start + k;
-  let artistRelFile = fs.createWriteStream('./database/relationsAll.csv');
-  for (var i = 0; i < 10; i ++) {
-    await genArtistRelationships(artistRelFile, start, end);
-    artistRelFile = fs.createWriteStream(`./database/relations${i}.csv`);
-    start += k; 
-    console.log('yoohoo')
-  }
-}
+// const genRelations = async () => {
+//   k = 1000000;
+//   start = 1;
+//   end = start + k;
+//   let artistRelFile = fs.createWriteStream('./database/relationsAll.csv');
+//   for (var i = 0; i < 10; i ++) {
+//     await genArtistRelationships(artistRelFile, start, end);
+//     artistRelFile = fs.createWriteStream(`./database/relations${i}.csv`);
+//     start += k; 
+//     console.log('yoohoo')
+//   }
+// }
 
-genRelations();
+// genRelations();
